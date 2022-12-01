b610ed4937e6899c3204acdf4beb8330e0f99821
Di Zhuang <dzhuang2@gmail.com>
2022-12-01 09:33:53 -0600

    initial commit

---
+++ src/bin/1.rs
@@ -0,0 +1,34 @@
+// https://adventofcode.com/2022/day/1
+// cargo run --bin 1
+
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let top: usize = args
+        .get(1)
+        .map_or(1, |x| x.parse::<usize>().expect("top X"));
+
+    let path = "input/1.txt";
+    let contents = fs::read_to_string(path).expect(&format!("Failed to read input from {}", path));
+
+    let input_data: Vec<Vec<u64>> = contents
+        .split("\n\n")
+        .map(|x| x.split("\n").map(|s| s.parse::<u64>().unwrap()).collect())
+        .collect();
+
+    // println!("input_data:\n{:?}", input_data);
+
+    let mut totals: Vec<u64> = input_data.iter().map(|xs| xs.iter().sum()).collect();
+
+    // sort in reverse
+    totals.sort_by(|x, y| y.cmp(&x));
+
+    println!(
+        "top {} carries a total of {} calories!",
+        top,
+        totals.iter().take(top).sum::<u64>()
+    );
+
+}
---
+++ src/bin/2.rs
@@ -0,0 +1,5 @@
+// https://adventofcode.com/2022/day/1
+
+fn main() {
+    println!("Hello, world! 2");
+}
